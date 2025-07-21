14839c3310cf80ea2c3c8fce38169eade143de51
Theeguycmd <sthembiso.theeguy@gmail.com>
2025-07-21 15:35:55 +0200

    added plates dir

---
+++ problemset2/plates/plates.py
@@ -0,0 +1,38 @@
+def main():
+    plate = input("Plate: ")
+    if is_valid(plate):
+        print("Valid")
+    else:
+        print("Invalid")
+
+
+def is_valid(s):
+    unwanted = "!#$%&'()*+,-./:;<=>?@[]^_`{|}~" + '"' + "\\"
+    numbers = ""
+    letters = ""
+
+    for i in s:
+        if i in unwanted:
+            return False
+        else:
+            continue
+
+    if len(s) >= 2 and len(s) <= 6:
+        letters = letters + s[0:2]
+        for j in s[2::]:
+            if j.isnumeric():
+                numbers = numbers + j
+            else:
+                letters = letters + j
+        if int(numbers[0]) == 0:
+            return False
+        else:
+            if letters + numbers == s:
+                return True
+            else:
+                return False
+    else:
+        return False
+
+
+main()
