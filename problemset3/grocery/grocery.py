a32400213420971d134131431a761c94319314cb
Theeguycmd <sthembiso.theeguy@gmail.com>
2025-07-30 20:20:36 +0200

    updated grocery by sorting a list in dictionaty

--- problemset3/grocery/grocery.py
+++ problemset3/grocery/grocery.py
@@ -2,20 +2,22 @@ def main():
     toBuy = {}
     while True:
         try:
-            item = input("").lower()
+            item = input("").strip().lower()
         except EOFError:
             print("\n")
             print("\n")
-            for i in toBuy:
+            for i in sorted(toBuy):
                 print(f"{toBuy[i]} {i.upper()}")
             break
         else:
-            if item.isspace() == True or item == "\n" or item == "\t" or item == "\r":
-                pass
+            if item == "":
+                continue
             elif item.upper() in toBuy:
                 toBuy[item.upper()] += 1
             else:
                 toBuy[item.upper()] = 1
+
+
 
 if __name__ == "__main__":
     main()
