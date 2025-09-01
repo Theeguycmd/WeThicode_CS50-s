88924a3925b26aee1e3dcf2be6d7ff5819530ac9
Theeguycmd <sthembiso.theeguy@gmail.com>
2025-09-01 12:01:00 +0200

    updated my numb3rs to make sure there were only 4 values separated by a full stop

--- problemset7/numb3rs/numb3rs.py
+++ problemset7/numb3rs/numb3rs.py
@@ -6,17 +6,17 @@ def main():
 def validate(arg):
     if  "." in arg:
         numbers = arg.split(".")
-        try:
-            for number in [int(num) for num in numbers]:
-                if 0 <=  number <= 255:
-                    continue
-                else:
-                    return False
-            return True
-        except ValueError:
-            return False
-    else:
-        return False
+        if len(numbers) == 4:
+            try:
+                for number in [int(num) for num in numbers]:
+                    if 0 <=  number <= 255:
+                        continue
+                    else:
+                        return False
+                return True
+            except ValueError:
+                return False
+    return False
 
 if __name__ == "__main__":
     main()
